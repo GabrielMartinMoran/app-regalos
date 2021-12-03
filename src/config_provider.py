cd4723565d571905a7b7267897ced951340eaefd
Gabriel Martín Moran <gabriel.moran@fluxit.com.ar>
2021-12-03 16:14:36 -0300

    Trying to fix static files serving

--- src/config_provider.py
+++ src/config_provider.py
@@ -2,10 +2,10 @@ import os
 
 
 class ConfigProvider:
-    CLIENT_APP_FOLDER = './web'
     RUN_DEBUG_MODE = os.environ.get('DEBUG_MODE', 'False').lower() == 'true'
     USE_RELOADER = os.environ.get('USE_RELOADER', 'False').lower() == 'true'
     APP_PORT = os.environ.get('APP_PORT', 5000)
     GOOGLE_JSON_TOKEN = os.environ.get('GOOGLE_JSON_TOKEN')
     GOOGLE_SHEET_DB_NAME = os.environ.get('GOOGLE_SHEET_DB_NAME')
     JWT_SECRET = os.environ.get('JWT_SECRET', 'jwt_insecure_secret')
+    CLIENT_APP_FOLDER = './web' if RUN_DEBUG_MODE else './src/web'
